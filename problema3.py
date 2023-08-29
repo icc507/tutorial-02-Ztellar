e4717ca08b1e67bfbc8b211dd09387c2fa802761
Ztellar <bsalamanca159@hotmail.com>
2023-08-29 10:10:01 -0400

    Update problema3.py

--- problema3.py
+++ problema3.py
@@ -14,10 +14,10 @@ def arbolTrinario(numero):
 
 
 def insertaEnArbolTrinario(arbol, numero):
-    if numero == arbol[0]:
-        insertaEnArbolTrinario(arbol[2], numero)
-    elif arbol == []:
+    if arbol == []:
         arbol += arbolTrinario(numero)
+    elif numero == arbol[0]:
+        insertaEnArbolTrinario(arbol[2], numero)
     elif numero <= arbol[0]:
         insertaEnArbolTrinario(arbol[1], numero)
     else:
